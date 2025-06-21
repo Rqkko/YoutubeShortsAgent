49e0d7f0abe3a4ecd63bce2e0284588ac5a2745a
Rqkko <rakko26@gmail.com>
2025-06-21 21:45:18 +0700

    feat: add scriptwriter_agent

--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -1,4 +1,14 @@
 from google.adk.agents import LlmAgent
 from google.adk.tools import google_search
 
-# from youtube_shorts_assistant.tools import load_instruction_from_file
+from .utils import load_instruction_from_file
+
+scriptwriter_agent = LlmAgent(
+    name="ShortsScriptwriter",
+    model="gemini-2.0-flash-001",
+    instruction=load_instruction_from_file("scriptwriter_instruction.txt"),
+    tools=[google_search],
+    output_key="generated_script",  # Save result to state
+)
+
+root_agent = scriptwriter_agent
